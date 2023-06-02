13d5a5d9d54af3612a5a848dcb6b5c8883973fd6
SergeyPC <SergeyPC@DESKTOP-TMGSBMF>
2023-06-02 13:29:16 +0300

    Добавьте файлы проекта.

---
+++ ExampleWork/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using ExampleWork;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+string path = "E:\\Задание\\Задание\\data.json"; 
+List<Worker> workers = new List<Worker>();
+workers = JsonConvert.DeserializeObject<List<Worker>>(File.ReadAllText(path));
+workers = workers.OrderByDescending(worker => worker.salary).ToList();
+foreach (Worker worker in workers)
+{
+    worker.phoneNumber = worker.phoneNumber.Insert(0, "+").Insert(2,"(").Insert(6, ")").Insert(10,"-").Insert(13,"-");
+}
+using (StreamWriter sw = new StreamWriter("output.txt", false, System.Text.Encoding.UTF8))
+{
+    for (int i = 0; i < 20; i++)
+    {
+        sw.WriteLine($"{workers[i].name} {workers[i].phoneNumber} {workers[i].salary}");
+    }
+}
---
+++ ExampleWork/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleWork
+{
+    public class Worker
+    {
+        public string name;
+        public string phoneNumber;
+        public int salary;
+    }
+}
